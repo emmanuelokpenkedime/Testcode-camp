6f635be74c9165229df75bd1f1a41875d5c56a1e
emmanuelokpenkedime <emmanuelokpenkedime@gmail.com>
2023-03-16 13:42:19 +0100

    Added Task 14 and Task 17

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        System.out.println("It is FANTASTIC!");
+        System.out.println("I LOVE SOFTWARE TESTING!");
+    }
+}
+
+
